71f18126683717ebd460b19e7849c075b8fa0645
Dev <dev@Roys-MacBook-Pro.local>
2022-07-19 03:12:08 +0500

    Corrected features migration

--- db/migrate/20220627215703_create_feature_planss.rb
+++ db/migrate/20220627215703_create_feature_planss.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class CreateFeaturePlans < ActiveRecord::Migration[5.2]
+class CreateFeaturePlanss < ActiveRecord::Migration[5.2]
   def change
     create_table :feature_plans do |t|
       t.references :plan, foreign_key: true
