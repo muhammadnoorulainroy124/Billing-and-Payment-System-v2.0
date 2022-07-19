3b80c6fa9a0386af17862181fee1a38aef168c49
Dev <dev@Roys-MacBook-Pro.local>
2022-07-19 14:28:39 +0500

    fixed amount in transaction table

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -7,8 +7,7 @@ StripeEvent.configure do |events|
   events.subscribe 'invoice.paid' do |event|
     plan_name = Stripe::Product.retrieve(event.data.object.lines.data[0].plan.product).name
     buyer_name = User.find_by(email: event.data.object.customer_email).name
-    puts "\n\n\n\n\n\n\n#{plan_name}\n\n\n\n\n\n\n#{buyer_name}\n\n\n\n\n\n\n"
-    Transaction.create(plan_name: plan_name, amount: event.data.object.amount_paid, buyer_name: buyer_name, buyer_email: event.data.object.customer_email)
+    Transaction.create(plan_name: plan_name, amount: event.data.object.amount_paid.to_i/100, buyer_name: buyer_name, buyer_email: event.data.object.customer_email)
     InvoiceMailer.with({ "customer_email": event.data.object.customer_email, "hosted_invoice_url": event.data.object.hosted_invoice_url, "invoice_pdf": event.data.object.invoice_pdf}).order_successful_email.deliver_now
   end
 end
